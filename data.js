401782ed3dca829079ae3345aa8e466febf2f777
Ali hasan <114381368+A-hasan-code@users.noreply.github.com>
2023-10-20 11:04:49 +0500

    change

--- data.js
+++ data.js
@@ -6,4 +6,9 @@
 //undefined
 //object
 let a = 33
-console.log(typeof a)
+console.log(typeof a) // conversation 
+let b = "12";
+console.log(typeof b)
+let change = Number(b);
+console.log(typeof change)
+    //string  conversation
--- var.js
+++ var.js
@@ -1,3 +1,7 @@
 var a = 20
 let age = 12;
 const username = "ali hasan"
+    // conversation 
+let b = "12";
+console.log(typeof b)
+let change = Number b;
